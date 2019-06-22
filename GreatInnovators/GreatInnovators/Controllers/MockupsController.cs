2a3d112874a16959bafb53a540f6669c5f5c9509
vkondyrev1474 <vkondyrev1474@gmail.com>
2019-06-22 21:25:27 +0700

    вот

---
+++ GreatInnovators/GreatInnovators/Controllers/MockupsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreatInnovators.Controllers
+{
+    public class MockupsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult AllForums()
+        {
+            return this.View();
+        }
+
+        public IActionResult SingleForum()
+        {
+            return this.View();
+        }
+
+        public IActionResult SingleTopic()
+        {
+            return this.View();
+        }
+
+        public IActionResult History()
+        {
+            return this.View();
+        }
+    }
+}
